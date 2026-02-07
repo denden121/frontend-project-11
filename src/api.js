16ea3d039e9ccd2fa42056bd6338310cf8668b3d
dgurachevskii <lisnik.job@gmail.com>
2026-02-07 22:09:08 +0300

    Исправил тесты

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const ALL_ORIGINS_RAW = 'https://allorigins.hexlet.app/raw';
+const ALL_ORIGINS_GET = 'https://allorigins.hexlet.app/get';
 
 const fetchFeed = (feedUrl) => {
-  const url = `${ALL_ORIGINS_RAW}?url=${encodeURIComponent(feedUrl)}&disableCache=true`;
+  const url = `${ALL_ORIGINS_GET}?url=${encodeURIComponent(feedUrl)}&disableCache=true`;
 
   return axios.get(url, {
     timeout: 10000,
-    responseType: 'text',
-  }).then((response) => response.data);
+    responseType: 'json',
+  }).then((response) => response.data.contents);
 };
 
 export default fetchFeed;
